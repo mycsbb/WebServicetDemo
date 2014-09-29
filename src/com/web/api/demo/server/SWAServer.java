a2b0163419a8ece2b23f7e81b1b6359d06aa4b0d
mycsbb <csbbvi@126.com>
2014-09-29 17:40:55 +0800

    init commit

---
+++ src/com/web/api/demo/server/SWAServer.java
@@ -0,0 +1,19 @@
+package com.web.api.demo.server;
+
+import com.web.api.demo.thread.SWAThread;
+
+public class SWAServer {
+
+	public void start() {
+		SWAThread thread135 = new SWAThread("13500", "135号码段发送测试", 100, 10030);
+		SWAThread thread136 = new SWAThread("13600", "136号码段发送测试", 100, 10030);
+		SWAThread thread137 = new SWAThread("13700", "136号码段发送测试", 100, 10030);
+		SWAThread thread138 = new SWAThread("13800", "136号码段发送测试", 100, 10030);
+		SWAThread thread139 = new SWAThread("13900", "136号码段发送测试", 100, 10030);
+		thread135.start();
+		thread136.start();
+		thread137.start();
+		thread138.start();
+		thread139.start();
+	}
+}
